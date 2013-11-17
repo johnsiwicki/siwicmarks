5a1b402924425ce55866ed838cd5b7f16cf35137
John Siwicki <johnsiwicki@gmail.com>
2013-11-16 20:21:16 -0500

    first commit

---
+++ app/models/meal.rb
@@ -0,0 +1,3 @@
+class Meal < ActiveRecord::Base
+  attr_accessible :calories, :food_name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siwicmarks::Application.config.secret_token = '00b7f6ad0aca12a17bea737aa7ecd2b19d5f6ac864ca60cea0716362ad531c97ec1f95d3e3bdf5445fa79a73b4584da3088ee0db297b1773c5b3f1087372838a'
